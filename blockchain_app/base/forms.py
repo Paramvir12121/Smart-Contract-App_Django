1dafe1be0b1932d1150e0c0fbb10da091b85a107
Paramvir12121 <paramvir12121@gmail.com>
2023-03-31 12:26:45 -0400

    form function

--- blockchain_app/base/forms.py
+++ blockchain_app/base/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from .models import Member
 
-class FormName(forms.Form):
-    name = forms.CharField()
-    email = forms.CharField()
-    account_id = forms.CharField()
+class MemberForm(forms.ModelForm):
+    class Meta:
+        model = Member
+        fields = ['fname','lname','email','account_id']
+    # name = forms.CharField()
+    # email = forms.CharField()
+    # account_id = forms.CharField()
--- blockchain_app/base/urls.py
+++ blockchain_app/base/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main/',views.mainPage, name='mainPage'),
-    #path('photos/<str:pk>/', views.viewPhoto,name='photo'),
-    path('signup/',views.signup, name='signup'),
     path('',views.home, name='home'),
+    path('main/',views.mainPage, name='mainPage'),
+    path('signup/',views.signup, name='signup'),
+    #path('photos/<str:pk>/', views.viewPhoto,name='photo'),
+    
 ]
